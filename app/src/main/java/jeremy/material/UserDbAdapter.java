cc87ee35a15ae315f19ebe338a840599ced5dff5
Jeremy Liu <j.liu1104@hotmail.com>
2016-05-27 18:24:41 +0800

    Add user select and search functions.

--- app/src/main/java/jeremy/material/UserDbAdapter.java
+++ app/src/main/java/jeremy/material/UserDbAdapter.java
@@ -7,6 +7,7 @@ import android.database.CursorIndexOutOfBoundsException;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.BitmapFactory;
 
 import java.util.List;
 
